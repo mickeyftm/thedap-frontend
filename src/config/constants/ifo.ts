bb721579473c5307fd1a5d2a9bacf2eb209ef2fb
your_username <luca.eich@amazingblocks.io>
2021-06-20 09:23:20 +0100

    add adress

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -1,7 +1,7 @@
 import tokens from './tokens'
 import { Ifo, Token } from './types'
 
-const cakeBnbLpToken: Token = {
+const usdtToken: Token = {
   symbol: tokens.usdt.symbol,
   address: tokens.usdt.address,
   decimals: 18,
@@ -10,33 +10,33 @@ const cakeBnbLpToken: Token = {
 export const ifos: Ifo[] = [
   {
     id: 'metagamehub',
-    address: '',
+    address: '0x4c590D0874aBd0c64Ed3477B0BAe731776A0f135',
     isActive: true,
     name: 'metagamehub (MGH)',
     poolEarly: {
-      saleAmount: '375,000 MGH',
-      raiseAmount: '$750,000',
-      cakeToBurn: '$375,000',
-      distributionRatio: 0.1,
+      saleAmount: '20,000,000 MGH',
+      raiseAmount: '$2,000,000',
+      cakeToBurn: '$0',
+      distributionRatio: 0.4,
     },
     poolBasic: {
-      saleAmount: '375,000 MGH',
-      raiseAmount: '$750,000',
-      cakeToBurn: '$375,000',
-      distributionRatio: 0.1,
+      saleAmount: '20,000,000 MGH',
+      raiseAmount: '$2,000,000',
+      cakeToBurn: '$0',
+      distributionRatio: 0.4,
     },
     poolUnlimited: {
-      saleAmount: '875,000 MGH',
-      raiseAmount: '$2,500,000',
-      cakeToBurn: '$1,250,000',
-      distributionRatio: 0.7,
+      saleAmount: '10,000,000 MGH',
+      raiseAmount: '$1,000,000',
+      cakeToBurn: '$0',
+      distributionRatio: 0.2,
     },
-    currency: cakeBnbLpToken,
+    currency: usdtToken,
     token: tokens.mgh,
     releaseBlockNumber: 7707736,
-    campaignId: '511110000',
+    campaignId: '100010000',
     articleUrl: 'https://metagamehub.io',
-    tokenOfferingPrice: 2.0,
+    tokenOfferingPrice: 0.1,
     version: 2,
   }
   /**
